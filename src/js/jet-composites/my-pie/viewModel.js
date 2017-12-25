8a287397f724343fb6b871e93d90f6789fc4e128
Mile <mingyaozhu26@126.com>
2017-12-25 15:51:01 +0800

    change language

--- src/js/jet-composites/my-pie/viewModel.js
+++ src/js/jet-composites/my-pie/viewModel.js
@@ -1,11 +1,11 @@
 /**
-  Copyright (c) 2015, 2017, Oracle and/or its affiliates.
-  The Universal Permissive License (UPL), Version 1.0
-*/
+ Copyright (c) 2015, 2017, Oracle and/or its affiliates.
+ The Universal Permissive License (UPL), Version 1.0
+ */
 define(
-    ['ojs/ojcore', 'knockout', 'jquery', 'ojs/ojchart'], function (oj, ko, $) {
+        ['ojs/ojcore', 'knockout', 'jquery', 'ojs/ojchart'], function (oj, ko, $) {
     'use strict';
-    
+
     function ExampleComponentModel(context) {
         var self = this;
         self.composite = context.element;
@@ -14,36 +14,44 @@ define(
         self.dataUrl = ko.observable('dataUrl');
         self.pieSeriesValue = ko.observableArray();
         self.threeDValue = ko.observable('off');
-        self.innerRadius=ko.observable(0);
-        self.centerLabel=ko.observable("");
+        self.innerRadius = ko.observable(0);
+        self.centerLabel = ko.observable("");
+
+        self.serToolValue = ko.observable();
+        self.valToolValue = ko.observable();
+        self.groupToolValue = ko.observable();
         context.props.then(function (propertyMap) {
             //Store a reference to the properties for any later use
-            
+
             self.properties = propertyMap;
-            
-            if(self.properties.chartName!=undefined){
+
+            if (self.properties.chartName != undefined) {
                 self.chartName(self.properties.chartName)
             }
-            if(self.properties.innerRadius!=undefined){
+            if (self.properties.innerRadius != undefined) {
                 console.log(self.properties.innerRadius)
                 self.innerRadius(self.properties.innerRadius)
             }
-            if(self.properties.centerLabel!=undefined){
+            if (self.properties.centerLabel != undefined) {
                 self.centerLabel(self.properties.centerLabel)
             }
-            if(self.properties.dataUrl!=undefined){
-                console.log('3333:'+self.properties.dataUrl);
+            if (self.properties.dataUrl != undefined) {
+                console.log('3333:' + self.properties.dataUrl);
                 self.dataUrl(self.properties.dataUrl);
-                $.getJSON( self.properties.dataUrl, function( data ) {
-                    self.pieSeriesValue(data)
-                  });
+                $.getJSON(self.properties.dataUrl, function (data) {
+                    self.serToolValue(data.seriesTooltip);
+                    self.valToolValue(data.valueTooltip);
+                    self.groupToolValue(data.groupTooltip);
+                    self.pieSeriesValue(data.dataArr)
+                });
             }
-            
+
             //Parse your component properties here 
 
         });
-    };
-    
+    }
+    ;
+
     //Lifecycle methods - uncomment and implement if necessary 
     //ExampleComponentModel.prototype.activated = function(context){
     //};
