53ddbdeca5e74f30f401b0cafdbb17a38942b67b
teliu <teliu@TELIU-CN.cn.oracle.com>
2017-12-28 11:23:31 +0800

    drilling2

--- src/js/jet-composites/my-funnel/viewModel.js
+++ src/js/jet-composites/my-funnel/viewModel.js
@@ -3,7 +3,7 @@
  The Universal Permissive License (UPL), Version 1.0
  */
 define(
-        ['ojs/ojcore', 'knockout', 'jquery', 'ojs/ojchart'], function (oj, ko, $) {
+        ['ojs/ojcore', 'knockout', 'jquery', 'ojs/ojchart', 'ojs/ojlabel', 'ojs/ojcollectiontabledatasource', 'ojs/ojtable', 'ojs/ojarraydataprovider', 'ojs/ojbutton', 'ojs/ojdialog', 'ojs/ojmoduleanimations'], function (oj, ko, $) {
     'use strict';
 
     function ExampleComponentModel(context) {
@@ -50,6 +50,40 @@ define(
             //Parse your component properties here 
 
         });
+        self.drillingTitle = ko.observable("表格");
+        var deptArray = [
+            {"ICID": 11, "number": "203", "names": "Tom", "catagory": "F10211"}
+        ];
+        self.DataObservableArray = ko.observableArray(deptArray);
+        self.dataprovider = new oj.ArrayDataProvider(self.DataObservableArray, {idAttribute: 'ICID'});
+
+        self.abnormalKeyDrillDown = function (event) {
+            self.DataObservableArray.removeAll();
+            console.log(event.detail);
+            var jsonData;
+            switch (event.detail.series) {
+                case "3（含）个月以上":
+                    jsonData = 'js/data/abnormal/drilling2/m3.json';
+                    break;
+                case "6（含）个月以上":
+                    jsonData = 'js/data/abnormal/drilling2/m6.json';
+                    break;
+                case "12（含）个月以上":
+                    jsonData = 'js/data/abnormal/drilling2/m12.json';
+                    break;
+                default:
+                    jsonData = 'js/data/abnormal/drilling2/m3.json';
+                    break;
+            }
+            self.drillingTitle("详细列表");
+
+            $.getJSON(jsonData,
+                    function (resp) {
+                        self.DataObservableArray(resp.arrayData);
+                        document.querySelector('#popDrillingChart2').open();
+                    });
+        };
+
     }
     ;
 
